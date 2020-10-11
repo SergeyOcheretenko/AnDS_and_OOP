69096ec49f914c1e631d27df26dd6b73576bebcd
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2020-10-11 15:39:30 +0300

    Update lab_1.2.c

--- Lab 1/lab_1.2.c
+++ Lab 1/lab_1.2.c
@@ -4,16 +4,14 @@ int main() {
     printf("Enter x:\n");
     double x;
     scanf("%lf", &x);
+    printf("x = %lf\n", x);
     if (x >= 0 && x <= 8 || x >= 16){
-        printf("x = %lf\n", x);
         printf("%lf", -3 * (x * x) / 5 + 9);
     }
     else if (x < -1){
-        printf("x = %lf\n", x);
         printf("%lf", 15 * x - 2);
     }
     else {
-        printf("x = %lf\n", x);
         printf("no value");
     }
     return 0;
