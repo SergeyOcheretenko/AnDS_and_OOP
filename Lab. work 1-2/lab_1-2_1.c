e16d63828a5c3c856286e172b7ecd1cf84dd82d2
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2020-11-03 12:45:01 +0200

    Create lab_1-2_1.c

---
+++ Lab. work 1-2/lab_1-2_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    printf("Enter n:\n");
+    int n;
+    double ceils = 1, floors, result = 0;
+    scanf("%d", &n);
+    printf("Your n:\n%d\n", n);
+    for (int i = 1; i <= n; i += 1){
+        for (int j = 1; j <= i; j += 1){
+            ceils *= (j + cos((double) j));
+        }
+        floors = pow(4, i) - i;
+        result += ceils / floors;
+    }
+    printf("Result:\n%lf", result);
+    return 0;
+}
