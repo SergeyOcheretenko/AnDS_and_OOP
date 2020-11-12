b87b8be93f07ee101ff4714ec175ad1140260e94
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2020-11-12 10:34:42 +0200

    Add counter in lab_1-2_1

--- Lab. work 1-2/lab_1-2_1.c
+++ Lab. work 1-2/lab_1-2_1.c
@@ -4,17 +4,21 @@
 int main(){
     printf("Enter n:\n");
     int n;
+    int counter = 0;
     double ceils = 1, floors, result = 0;
     scanf("%d", &n);
     printf("Your n:\n%d\n", n);
     for (int i = 1; i <= n; i += 1){
         for (int j = 1; j <= i; j += 1){
             ceils *= (j + cos((double) j));
+            counter += 3;
         }
         floors = pow(4, i) - i;
         result += ceils / floors;
         ceils = 1;
+        counter += i + 2;
     }
-    printf("Result:\n%lf", result);
+    printf("Result:\n%lf\n", result);
+    printf("Counter: %d", counter);
     return 0;
 }
