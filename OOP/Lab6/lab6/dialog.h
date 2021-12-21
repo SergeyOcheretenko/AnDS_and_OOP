4bd89edce97e2993712c63493c92e1497afdb2fa
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2021-12-21 20:14:38 +0200

    Upload lab6 files

---
+++ OOP/Lab6/lab6/dialog.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define IDD_LAB6_DIALOG                 102
+#define IDC_N                           1000
+#define IDB_COUNT                       1001
+#define IDC_MIN                         1002
+#define IDC_MAX                         1003
+
+extern int Dialog(HINSTANCE hInst, HWND hWnd);
